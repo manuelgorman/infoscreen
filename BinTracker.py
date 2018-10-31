e5311571fac2063a1707821dd9cb2544de862481
Manuel Gorman <manuelgorman@hotmail.com>
2018-10-31 00:32:20 +0000

    Converted indentations to Tabs, updated .gitignore

--- BinTracker.py
+++ BinTracker.py
@@ -3,55 +3,55 @@ import json, logging
 from CoreLib import CoreLib,DataSource
 
 class BinTracker:
-    """Class for displaying Gorgie Road bin information"""
-    
-    title = "Bin Collection"
+	"""Class for displaying Gorgie Road bin information"""
+	
+	title = "Bin Collection"
 
-    def __init__(self,file):
-            fileH = open(file, "r")
-            txt = fileH.read()
-            self.binDict = json.loads(txt)
-            fileH.close()
-            self.dayOfWeek = self.binDict["dayofweek"]
-            self.title = "Bin Collections"
-            logging.debug("[BinTracker] Initialized")
-            
+	def __init__(self,file):
+			fileH = open(file, "r")
+			txt = fileH.read()
+			self.binDict = json.loads(txt)
+			fileH.close()
+			self.dayOfWeek = self.binDict["dayofweek"]
+			self.title = "Bin Collections"
+			logging.debug("[BinTracker] Initialized")
+			
 
-    def next_weekday(self,date):
-        """ Date: after which to find the next day. Weekday: int for which day you wish to find, 0=Monday, 6=Sunday"""
-        days_ahead = self.dayOfWeek - date.weekday()
-        if days_ahead <= 0:
-            days_ahead += 7
-        return date + datetime.timedelta(days_ahead)
-            
-    def get_next_collection(self):
-        currentMonth, currentDay = datetime.datetime.now().strftime("%m"), datetime.datetime.now().strftime("%d")
-        next_wed = self.next_weekday(datetime.datetime.now())
-        next_wed_day = next_wed.strftime("%d")
-        next_wed_month = next_wed.strftime("%m")
-        logging.debug("[+] Checking %s/%s" % (next_wed_day,next_wed_month))
-        bins = []
-        for binType in self.binDict["groups"][self.binDict[next_wed_month][next_wed_day]]:
-            bins.append(self.binDict["bins"][binType])
-            logging.debug("[~] " + self.binDict["bins"][binType])
-        return bins
+	def next_weekday(self,date):
+		""" Date: after which to find the next day. Weekday: int for which day you wish to find, 0=Monday, 6=Sunday"""
+		days_ahead = self.dayOfWeek - date.weekday()
+		if days_ahead <= 0:
+			days_ahead += 7
+		return date + datetime.timedelta(days_ahead)
+			
+	def get_next_collection(self):
+		currentMonth, currentDay = datetime.datetime.now().strftime("%m"), datetime.datetime.now().strftime("%d")
+		next_wed = self.next_weekday(datetime.datetime.now())
+		next_wed_day = next_wed.strftime("%d")
+		next_wed_month = next_wed.strftime("%m")
+		logging.debug("[+] Checking %s/%s" % (next_wed_day,next_wed_month))
+		bins = []
+		for binType in self.binDict["groups"][self.binDict[next_wed_month][next_wed_day]]:
+			bins.append(self.binDict["bins"][binType])
+			logging.debug("[~] " + self.binDict["bins"][binType])
+		return bins
 
-    def getData(self):
-        data = self.get_next_collection()
-        dataString = ""
-        for collection in data:
-            if data.index(collection) == len(data)-1:
-                dataString += collection
-            else:
-                dataString += collection+", "
+	def getData(self):
+		data = self.get_next_collection()
+		dataString = ""
+		for collection in data:
+			if data.index(collection) == len(data)-1:
+				dataString += collection
+			else:
+				dataString += collection+", "
 
-        return [[self.title, dataString]]
+		return [[self.title, dataString]]
 
 
 
 if __name__ == "__main__":
-    print("[BinTracker] Running Standalone")
-    bt = BinTracker("binschedule.json") 
-    print(bt.get_next_collection())
-    print(bt.next_weekday(datetime.datetime.now()))
-    print (bt.getData())
+	print("[BinTracker] Running Standalone")
+	bt = BinTracker("binschedule.json") 
+	print(bt.get_next_collection())
+	print(bt.next_weekday(datetime.datetime.now()))
+	print (bt.getData())
--- LothianBusTracker.py
+++ LothianBusTracker.py
@@ -3,77 +3,77 @@ from hashlib import md5
 from datetime import datetime
 
 class LothianBusTracker:
-    """Class for retrieving and displaying next bus info for Lothian Buses"""
-    
+	"""Class for retrieving and displaying next bus info for Lothian Buses"""
+	
 
-    def __init__(self,token):
-            self.apiToken = token
-            self.requestUrl = "http://ws.mybustracker.co.uk"
-            print("[BusTracker] Initialized")
-            
-    def makeRequest(self, functionName, args=None):
-            """Makes request to LB """
+	def __init__(self,token):
+			self.apiToken = token
+			self.requestUrl = "http://ws.mybustracker.co.uk"
+			print("[BusTracker] Initialized")
+			
+	def makeRequest(self, functionName, args=None):
+			"""Makes request to LB """
 
-            #Build and hash API token
-            print("[BusTracker(MakeRequest)] Computing MD5 token")
-            date = datetime.now().strftime("%Y%m%d%H")
-            tokenString = self.apiToken+date
-            md5Token = md5(bytes(tokenString, encoding='utf-8')).hexdigest()
-            print("[BusTracker(MakeRequest)] Timestamp:", date)
-            
+			#Build and hash API token
+			print("[BusTracker(MakeRequest)] Computing MD5 token")
+			date = datetime.now().strftime("%Y%m%d%H")
+			tokenString = self.apiToken+date
+			md5Token = md5(bytes(tokenString, encoding='utf-8')).hexdigest()
+			print("[BusTracker(MakeRequest)] Timestamp:", date)
+			
 
 
-            #Send request
-            mainArgs = {"module":"json", "key":md5Token, "function":functionName}
-            if args != None:
-                payload = {**args, **mainArgs}
-            else:
-                payload = mainArgs
-                
-            print("[BusTracker(MakeRequest,%s)] %s | %s" % (self.requestUrl,functionName,args))
-            r = requests.post(self.requestUrl, data=payload)
-            return r.json()
-            
-            
-    def getNextBuses(self,stopcodes):
-        """Takes a list of stopcodes and returns a dictionary of services and their arrival times"""
-        stops = {}
-        i = 1
-        for stopcode in stopcodes:
-            print("[BusTracker(NextBuses)] Added stop:", str(stopcode))
-            stops["stopId"+str(i)] = stopcode
-            i += 1
-        print("[BusTracker(NextBuses)] Requesting Data...")
-        returned_info = self.makeRequest("getBusTimes",args=stops)
-        buses = {}
-        print("[BusTracker(NextBuses)] Processing Data...")
-        for service in returned_info["busTimes"]:
-            buses[service["mnemoService"]] = []
-            for individual_bus in service["timeDatas"]:
-                buses[service["mnemoService"]].append(individual_bus["minutes"])                                             
-        return buses
+			#Send request
+			mainArgs = {"module":"json", "key":md5Token, "function":functionName}
+			if args != None:
+				payload = {**args, **mainArgs}
+			else:
+				payload = mainArgs
+				
+			print("[BusTracker(MakeRequest,%s)] %s | %s" % (self.requestUrl,functionName,args))
+			r = requests.post(self.requestUrl, data=payload)
+			return r.json()
+			
+			
+	def getNextBuses(self,stopcodes):
+		"""Takes a list of stopcodes and returns a dictionary of services and their arrival times"""
+		stops = {}
+		i = 1
+		for stopcode in stopcodes:
+			print("[BusTracker(NextBuses)] Added stop:", str(stopcode))
+			stops["stopId"+str(i)] = stopcode
+			i += 1
+		print("[BusTracker(NextBuses)] Requesting Data...")
+		returned_info = self.makeRequest("getBusTimes",args=stops)
+		buses = {}
+		print("[BusTracker(NextBuses)] Processing Data...")
+		for service in returned_info["busTimes"]:
+			buses[service["mnemoService"]] = []
+			for individual_bus in service["timeDatas"]:
+				buses[service["mnemoService"]].append(individual_bus["minutes"])                                             
+		return buses
 
-    def getUNextBuses(self,stopcodes):
-        """Takes a list of stopcodes and returns a unique dictionary of services and their arrival times"""
-        stops = {}
-        i = 1
-        for stopcode in stopcodes:
-            print("[BusTracker(UNextBuses)] Added stop:", str(stopcode))
-            stops["stopId"+str(i)] = stopcode
-            i += 1
-        print("[BusTracker(UNextBuses)] Requesting Data...")
-        returned_info = self.makeRequest("getBusTimes",args=stops)
-        buses = {}
-        print("[BusTracker(UNextBuses)] Processing Data...")
-        for service in returned_info["busTimes"]:
-            buses[service["mnemoService"]] = []
-            buses[service["mnemoService"]].append(service["timeDatas"][0]["minutes"])                                             
-        return buses
+	def getUNextBuses(self,stopcodes):
+		"""Takes a list of stopcodes and returns a unique dictionary of services and their arrival times"""
+		stops = {}
+		i = 1
+		for stopcode in stopcodes:
+			print("[BusTracker(UNextBuses)] Added stop:", str(stopcode))
+			stops["stopId"+str(i)] = stopcode
+			i += 1
+		print("[BusTracker(UNextBuses)] Requesting Data...")
+		returned_info = self.makeRequest("getBusTimes",args=stops)
+		buses = {}
+		print("[BusTracker(UNextBuses)] Processing Data...")
+		for service in returned_info["busTimes"]:
+			buses[service["mnemoService"]] = []
+			buses[service["mnemoService"]].append(service["timeDatas"][0]["minutes"])                                             
+		return buses
 
 if __name__ == "__main__":
-    print("[BusTracker] Running Standalone")
-    lb = LothianBusTracker("xxxxxxx")
-    buses = lb.getUNextBuses([36232687,36232624])
-    #json = lb.makeRequest("getBusTimes",args={"stopId1":36232687})
-    #print(json) 
-    print(buses)
+	print("[BusTracker] Running Standalone")
+	lb = LothianBusTracker("xxxxxxx")
+	buses = lb.getUNextBuses([36232687,36232624])
+	#json = lb.makeRequest("getBusTimes",args={"stopId1":36232687})
+	#print(json) 
+	print(buses)
